8bd19c7fd93a43303137c34acdf175da8d5b0fda
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-07-03 08:10:31 +0300

    test(IHB-5): add test script for pre commit and push

--- server/tests/middleware/GlobalExceptionMiddleware.test.ts
+++ server/tests/middleware/GlobalExceptionMiddleware.test.ts
@@ -1,22 +1,24 @@
 import { NextFunction, Response } from 'express';
 import GlobalExceptionMiddleware from '@middleware/GlobalExceptionMiddleware';
 
-it('should call res.status and res.json when an error occurs', () => {
-  const err = new Error('Test error') as any;
-  err.code = undefined; // Simulate no code property
-  const req = { originalUrl: '/test', method: 'GET', ip: '127.0.0.1' } as any;
-  const res = {
-    status: jest.fn().mockReturnThis(),
-    json: jest.fn(),
-  } as unknown as Response;
-  const next = jest.fn() as NextFunction;
+describe('GlobalExceptionMiddleware', () => {
+  it('should call res.status and res.json when an error occurs', () => {
+    const err = new Error('Test error') as any;
+    err.code = undefined; // Simulate no code property
+    const req = { originalUrl: '/test', method: 'GET', ip: '127.0.0.1' } as any;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+    const next = jest.fn() as NextFunction;
 
-  const middleware = new GlobalExceptionMiddleware();
-  middleware.allExceptionHandler(err, req, res, next);
+    const middleware = new GlobalExceptionMiddleware();
+    middleware.allExceptionHandler(err, req, res, next);
 
-  expect(res.status).toHaveBeenCalledWith(500);
-  expect(res.json).toHaveBeenCalledWith({
-    status: 'error',
-    message: 'Test error',
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      status: 'error',
+      message: 'Test error',
+    });
   });
 });
