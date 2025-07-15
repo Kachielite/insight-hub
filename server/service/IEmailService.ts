efc20ee469539f93206cdf2141e1657c9ab97720
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-07-15 06:02:51 +0300

    feat(IHB-7): update email service to include method to send project invite emails

--- server/service/IEmailService.ts
+++ server/service/IEmailService.ts
@@ -1,3 +1,9 @@
 export interface IEmailService {
   sendPasswordResetEmail(email: string, resetLink: string): Promise<void>;
+  sendEmailProjectInvite(
+    email: string,
+    inviterName: string,
+    projectName: string,
+    inviteLink: string
+  ): Promise<void>;
 }
